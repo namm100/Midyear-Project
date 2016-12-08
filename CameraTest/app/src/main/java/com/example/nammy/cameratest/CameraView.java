050d7831cab09c7de033555973d9c8683af08b33
namm100 <namm0@icloud.com>
2016-12-08 00:39:28 -0500

    Added stuff in Manifest

---
+++ CameraTest/app/src/main/java/com/example/nammy/cameratest/CameraView.java
@@ -0,0 +1,18 @@
+package com.example.nammy.cameratest;
+
+import android.content.Context;
+import android.graphics.Camera;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by nammy on 12/8/16.
+ */
+
+public class CameraView extends SurfaceView {
+    public CameraView(Context context, Camera camera) {
+        super(context);
+    }
+
+
+}
